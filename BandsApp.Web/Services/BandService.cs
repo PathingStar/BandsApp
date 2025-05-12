4fe78338a5b0372162e6ec581bad770349a4a028
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-05-12 16:39:57 +0200

    Bands Services inplemented

---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,24 @@
+﻿using BandsApp.Web.Models;
+namespace BandsApp.Web.Services;
+
+public class BandService
+{
+    private readonly List<Band> _bands;
+    public BandService()
+    {
+        _bands = new List<Band>
+        {
+            new Band { Id = 1, Name = "Band 1", Description = "Description 1" },
+            new Band { Id = 2, Name = "Band 2", Description = "Description 2" },
+            new Band { Id = 3, Name = "Band 3", Description = "Description 3" }
+        };
+    }
+    public Band[] GetAllBands()
+    {
+        return _bands.ToArray();
+    }
+    public Band? GetBandById(int id)
+    {
+        return _bands.FirstOrDefault(b => b.Id == id);
+    }
+}

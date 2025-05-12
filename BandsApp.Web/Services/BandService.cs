099ef4ecbb506e272b7cedc885638e1ec9105c5e
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-05-12 16:42:28 +0200

    Added my band

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -8,8 +8,9 @@ public class BandService
     {
         _bands = new List<Band>
         {
-            new Band { Id = 1, Name = "Band 1", Description = "Description 1" },
+            new Band { Id = 1, Name = "Disturbed", Description = "Disturbed är ett amerikanskt metalband som grundades 1994 under namnet Brawl av Dan Donegan, Steve \"Fuzz\" Kmak, Mike Wengren och Erich Awalt i Chicago, Illinois. David Draiman övertog som sångare 1996 och bandet bytte namn till Disturbed. Sedan bandet grundades har de sålt över 10 miljoner album och även hunnit spela med stora namn som Pendulum Garden och Burning Machine. " },
             new Band { Id = 2, Name = "Band 2", Description = "Description 2" },
+            new Band { Id = 3, Name = "Band 3", Description = "Description 3" },
             new Band { Id = 3, Name = "Band 3", Description = "Description 3" }
         };
     }

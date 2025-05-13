159b3b3db2974e4403034dc1295dcd5655b1d54d
Peter Ahlgren <ahlgren1234@gmail.com>
2025-05-13 09:38:03 +0200

    Add Details Views.

---
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,25 @@
+﻿using BandsApp.Web.Models;
+using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Controllers
+{
+    public class BandsController : Controller
+    {
+        BandService bandService = new BandService();
+
+        [Route("")]
+        public ActionResult Index()
+        {
+            var model = bandService.GetAllBands();
+            return View(model);
+        }
+
+        [Route("details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var model = bandService.GetBandById(id);
+            return View(model);
+        }
+    }
+}

d23ab4efcf958bfc1ceb0a63808f164a282bee30
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-05-13 10:13:01 +0200

    Img work in progres

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -5,4 +5,6 @@ public class Band
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+
+    public string ImageUrl { get; set; }
 }
--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -8,10 +8,10 @@ public class BandService
     {
         _bands = new List<Band>
         {
-            new Band { Id = 1, Name = "Disturbed", Description = "Disturbed är ett amerikanskt metalband som grundades 1994 under namnet Brawl av Dan Donegan, Steve \"Fuzz\" Kmak, Mike Wengren och Erich Awalt i Chicago, Illinois. David Draiman övertog som sångare 1996 och bandet bytte namn till Disturbed. Sedan bandet grundades har de sålt över 10 miljoner album och även hunnit spela med stora namn som Pendulum Garden och Burning Machine. " },
-            new Band { Id = 2, Name = "Guns n Roses", Description = "Guns N' Roses[a] is an American hard rock band formed in Los Angeles, California in 1985 as a merger of local bands L.A. Guns and Hollywood Rose." },
-            new Band { Id = 3, Name = "Sabaton", Description = "Sabaton is a Swedish power metal band from Falun. The band was formed in 1999. The vast majority of their songs are about historical events, mainly wars and significant battles." },
-            new Band { Id = 4, Name = "Nightwish", Description = "Nightwish är ett symfoniskt metalband från Kitee, Finland, grundat 1996 av Tuomas Holopainen, Emppu Vuorinen och Tarja Turunen. Bandet blev snabbt känt för sin unika kombination av opera-liknande sång och metal. De har släppt flera kritikerrosade album och turnerat världen över. Nightwish räknas som ett av de mest framgångsrika banden inom sin genre." }
+            new Band { Id = 1, ImageUrl="~/Img/20250218_disturbedpress_1.webp", Name = "Disturbed", Description = "Disturbed är ett amerikanskt metalband som grundades 1994 under namnet Brawl av Dan Donegan, Steve \"Fuzz\" Kmak, Mike Wengren och Erich Awalt i Chicago, Illinois. David Draiman övertog som sångare 1996 och bandet bytte namn till Disturbed. Sedan bandet grundades har de sålt över 10 miljoner album och även hunnit spela med stora namn som Pendulum Garden och Burning Machine. " },
+            new Band { Id = 2, ImageUrl="~/Img/guns-n-roses-at-uic-pavillion-chicago-illinois-august-21-1987.webp", Name = "Guns n Roses", Description = "Guns N' Roses[a] is an American hard rock band formed in Los Angeles, California in 1985 as a merger of local bands L.A. Guns and Hollywood Rose." },
+            new Band { Id = 3, ImageUrl="~/Img/910ab4a0-d5cb-424c-9c5c-49753160a86a.avif", Name = "Sabaton", Description = "Sabaton is a Swedish power metal band from Falun. The band was formed in 1999. The vast majority of their songs are about historical events, mainly wars and significant battles." },
+            new Band { Id = 4, ImageUrl="~/Img/61bf384bbb1f2f4a8021d112_opengraph copy.jpg", Name = "Nightwish", Description = "Nightwish är ett symfoniskt metalband från Kitee, Finland, grundat 1996 av Tuomas Holopainen, Emppu Vuorinen och Tarja Turunen. Bandet blev snabbt känt för sin unika kombination av opera-liknande sång och metal. De har släppt flera kritikerrosade album och turnerat världen över. Nightwish räknas som ett av de mest framgångsrika banden inom sin genre." }
         };
     }
     public Band[] GetAllBands()
@@ -20,6 +20,7 @@ public class BandService
     }
     public Band? GetBandById(int id)
     {
-        return _bands.FirstOrDefault(b => b.Id == id);
+        
+        return _bands.SingleOrDefault(b => b.Id == id);
     }
 }
